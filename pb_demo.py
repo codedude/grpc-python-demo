e6c5da70747c6087b0c66f76a118f0df864bd6e7
codedude <codedude@users.noreply.github.com>
2026-01-21 22:40:29 -0500

    add optional demo + map type

--- pb_demo.py
+++ pb_demo.py
@@ -46,5 +46,23 @@ def main():
     return 0
 
 
+def optional_field():
+    report = ReportResponse()
+    report.measures.add(
+        temp=25.0,
+        humidity=50,
+        # pressure=0,
+    )
+    pb_bin_out = report.SerializeToString()
+    new_report = ReportResponse()
+    new_report.ParseFromString(pb_bin_out)
+    try:
+        print(new_report.measures[0].pressure)
+        print(new_report.measures[0].HasField("pressure"))
+    except ValueError as e:
+        print(e)
+
+
 if __name__ == "__main__":
     sys.exit(main())
+    # sys.exit(optional_field())
